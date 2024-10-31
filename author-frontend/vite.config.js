c8a4a839fc4c62986ef0dd00d87bdc8402b22236
Nick <nickmufson1@gmail.com>
2024-10-31 14:55:42 -0400

    update author vite config

--- author-frontend/vite.config.js
+++ author-frontend/vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        'dompurify',
+        'prop-types',
+        'react-router-dom',
+        '@mdi/js',
+        '@mdi/react',
+        'jwt-decode',
+        '@tinymce/tinymce-react',
+        'jsonwebtoken'
+      ],
+      output: {
+        globals: {
+          'dompurify': 'DOMPurify',
+          'prop-types': 'PropTypes',
+          'react-router-dom': 'ReactRouterDOM',
+          '@mdi/js': 'mdiJs',
+          '@mdi/react': 'mdiReact',
+          'jwt-decode': 'jwtDecode',
+          '@tinymce/tinymce-react': 'TinyMCE',
+          'jsonwebtoken': 'jwt'
+        }
+      }
+    }
+  }
+});
