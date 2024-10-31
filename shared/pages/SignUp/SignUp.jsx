6dfcf461d2375c3ad45cd119cc103c5ddc485903
Nick <nickmufson1@gmail.com>
2024-10-31 14:46:30 -0400

    update peer dependencies

--- shared/pages/SignUp/SignUp.jsx
+++ shared/pages/SignUp/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { validateField } from './SignUp';
 import { signUpUser } from '../../services/signUpService';
-// import DOMPurify from 'isomorphic-dompurify';
+import DOMPurify from 'isomorphic-dompurify';
 import LabelInput from '../../../author-frontend/src/components/LabelInput/LabelInput';
 import styles from './SignUp.module.css';
 
@@ -29,12 +29,10 @@ const SignUp = () => {
   const handleChange = (e) => {
     const { name, value } = e.target;
 
-    // Update form input values
     setFormData((prevData) => ({
       ...prevData,
-      // to prevent XSS
-      [name]: value,
-      // [name]: DOMPurify.sanitize(value),
+
+      [name]: DOMPurify.sanitize(value),
     }));
 
     if (touchedFields[name]) {
