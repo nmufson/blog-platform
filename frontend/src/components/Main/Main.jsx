5ae6a2bd9dfd31972d238923a7ae15ec7398bba4
Nick <nickmufson1@gmail.com>
2024-11-01 16:47:21 -0400

    add vercel.json to frontend

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const Main = ({ children }) => <main>{children}</main>;
 
 Main.propTypes = {
-  children: PropTypes.node.isRequired, // React nodes to be rendered
+  children: PropTypes.node.isRequired,
 };
 
 export default Main;
